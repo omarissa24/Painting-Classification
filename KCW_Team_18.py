4e9dbd6e4add2e7ff8c65be4bb9cdf7aed93f4bb
Omar Issa <omarissa@oryxdentalsoftware.com>
2024-05-05 18:57:49 +0200

    cleanup

--- KCW_Team_18.py
+++ KCW_Team_18.py
@@ -7,50 +7,7 @@ from itertools import combinations, product
 from scipy.optimize import linear_sum_assignment
 import numpy as np
 
-# def process_paintings(file_path, is_binary=False):
-#     # a dictionary that stores the set of tags for each index of landscpe paintings
-#     landscape_tags = {}
-#     # a dictionary that stores the set of tags for each index of portrait paintings
-#     portrait_tags = {}
-#     # a dictionary that stores the set of frameglasses that contain a particular tag
-#     tag_frameglasses = {}
-#     with open(file_path, 'r') as file:
-#         n = int(next(file).strip())
-        
-#         frameglasses = []
-#         portrait_buffer = None
-        
-#         for i in range(n):
-#             line = next(file).strip().split()
-#             painting_type = line[0]
-#             tags = set(line[2:int(line[1])+2])
-            
-#             if painting_type == 'L':
-#                 landscape_tags[i] = tags
-#                 frameglasses.append({'type': 'L', 'paintings': [i], 'tags': list(tags)})
-#             elif painting_type == 'P':
-#                 portrait_tags[i] = tags
-#                 if portrait_buffer:
-#                     combined_tags = portrait_buffer['tags'].union(tags)
-#                     frameglasses.append({'type': 'P', 'paintings': [portrait_buffer['index'], i], 'tags': list(combined_tags)})
-#                     portrait_buffer = None
-#                 else:
-#                     portrait_buffer = {'tags': tags, 'index': i}
-            
-#             for tag in tags:
-#                 if tag not in tag_frameglasses:
-#                     tag_frameglasses[tag] = []
-#                 tag_frameglasses[tag].append(i)
-        
-#         if portrait_buffer:
-#             frameglasses.append({'type': 'P', 'paintings': [portrait_buffer['index']], 'tags': list(portrait_buffer['tags'])})
-
-#     # if not is_binary:
-#     #     frameglasses.sort(key=lambda x: len(x['tags']), reverse=True)
-#         # frameglasses.sort(key=lambda x: len(x['tags']), reverse=True)
-#     return frameglasses, landscape_tags, portrait_tags, tag_frameglasses
-
-def process_paintings(file_path):
+def process_paintings(file_path, is_binary=False):
     # a dictionary that stores the set of tags for each index of landscpe paintings
     landscape_tags = {}
     # a dictionary that stores the set of tags for each index of portrait paintings
@@ -72,7 +29,6 @@ def process_paintings(file_path):
                 frameglasses.append({'type': 'L', 'paintings': [i], 'tags': list(tags)})
             elif painting_type == 'P':
                 portrait_tags[i] = tags
-                # frameglasses.append({'type': 'P', 'paintings': [i], 'tags': list(tags)})
             
             for tag in tags:
                 if tag not in tag_frameglasses:
@@ -82,8 +38,7 @@ def process_paintings(file_path):
         merged_portraits = merge_portraits(portrait_tags, 2000)
         frameglasses.extend(merged_portraits)
 
-    # frameglasses = merge_portraits(frameglasses)
-    frameglasses.sort(key=lambda x: len(x['tags']), reverse=True)
+        frameglasses.sort(key=lambda x: len(x['tags']), reverse=True)
     return frameglasses, landscape_tags, portrait_tags, tag_frameglasses
 
 def merge_portraits(portrait_tags, batch_size=800):
@@ -123,53 +78,12 @@ def merge_portraits(portrait_tags, batch_size=800):
 
     return merged_portraits
 
-def get_freq_of_tags_with_index_of_frameglass(frameglass_combos):
-    tags_fg = {}
-    for i, fg in enumerate(frameglass_combos):
-        if len(fg['tags']) not in tags_fg:
-            tags_fg[len(fg['tags'])] = 0
-        tags_fg[len(fg['tags'])] += 1
-
-    # sort the values of the dictionary in descending order
-    tags_fg = dict(sorted(tags_fg.items(), key=lambda x: x[1], reverse=True))
-    return tags_fg
-
 def get_local_robotic_satisfaction(frameglass1, frameglass2):
     common_tags = len(set(frameglass1['tags']).intersection(set(frameglass2['tags'])))
     tags_in_frameglass1 = len(set(frameglass1['tags']).difference(set(frameglass2['tags'])))
     tags_in_frameglass2 = len(set(frameglass2['tags']).difference(set(frameglass1['tags'])))
     
     return min(common_tags, tags_in_frameglass1, tags_in_frameglass2)
-
-def get_max_satisfaction(frameglass_combos):
-
-    if not frameglass_combos:
-        return 0, []
-    
-    curr_fg = frameglass_combos[0]
-    res = [curr_fg]
-    rem_fg = frameglass_combos[1:]
-    total_satisfaction = 0
-
-    while rem_fg:
-        max_satisfaction = 0
-        max_fg = None
-
-        for fg in rem_fg:
-            satisfaction = get_local_robotic_satisfaction(curr_fg, fg)
-            if satisfaction >= max_satisfaction:
-                max_satisfaction = satisfaction
-                max_fg = fg
-
-        if max_fg:
-            res.append(max_fg)
-            rem_fg.remove(max_fg)
-            total_satisfaction += max_satisfaction
-            curr_fg = max_fg
-        else:
-            break
-
-    return total_satisfaction, res
 
 def get_max_satisfaction_batch(frameglass_combos, chunk_size=100):
     if not frameglass_combos:
@@ -315,35 +229,43 @@ def run_genetic_algorithm(toolbox, threshold=0.01, ngen=20, patience=5):
 #     optimal_batch_size = run_genetic_algorithm(toolbox, threshold=0.01, patience=10)
 #     print("Optimal Batch Size Found:", optimal_batch_size)
     
-def main(input_file_path, is_binary=False):
+def main(input_file_path, is_binary=False, is_oily=False, is_random=False, is_computable=False, is_example=False):
 
-    start = time.time()
     input_file_path = 'Data/' + input_file_path
     paintings, landscape_tags, portrait_tags, tag_frameglasses = process_paintings(input_file_path)
 
+    max_satisfaction = 0
+    max_satisfaction_combo = []
+
+    if is_example:
+        # example combo
+        max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(paintings, 10)
+
+    if is_computable:
+        # computable combo
+        max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(paintings, 1000)
+
     if is_binary:
+        # binary combo
         max_satisfaction, max_satisfaction_combo = best_combo_binary(paintings, tag_frameglasses)
-
-        output_file_path = str(max_satisfaction) + '-' + input_file_path.split('/')[-1].replace('.txt', '_output.txt')
-        write_output_file(output_file_path, max_satisfaction_combo)
-
-        print("Time taken:", (time.time() - start) / 60)
 
         return max_satisfaction
     
-    # max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(paintings, 1000)
-    # print("First batch done:", max_satisfaction)
-    # max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(max_satisfaction_combo, 1200)
+    if is_oily:
+        # oily combo
+        max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(paintings, 1000)
+        print("First batch done:", max_satisfaction)
+        max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(max_satisfaction_combo, 1200)
 
-    max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(paintings, 600)
-    print("First batch done:", max_satisfaction)
-    max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(max_satisfaction_combo, 1500)
+    if is_random:
+        # randomizing combo
+        max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(paintings, 1000)
+        print("First batch done:", max_satisfaction)
+        max_satisfaction, max_satisfaction_combo = get_max_satisfaction_batch(max_satisfaction_combo, 1500)
 
-    output_file_path = str(max_satisfaction) + '-' + input_file_path.split('/')[-1].replace('.txt', '_output.txt')
-    write_output_file(output_file_path, max_satisfaction_combo)
-
-    print("Time taken:", (time.time() - start) / 60)
+    # output_file_path = str(max_satisfaction) + '-' + input_file_path.split('/')[-1].replace('.txt', '_output.txt')
+    # write_output_file(output_file_path, max_satisfaction_combo)
 
     return max_satisfaction
 
-print(main('11_randomizing_paintings.txt'))
+print(main('110_oily_portraits.txt', is_oily=True))
--- KCW_final_score_18.py
+++ KCW_final_score_18.py
@@ -2,7 +2,28 @@ import time
 from KCW_Team_18 import main
 
 def KCW_final_score(input_file_path):
-    res = main(input_file_path)
+
+    res = 0
+
+    is_example = False
+    is_binary = False
+    is_computable = False
+    is_random = False
+    is_oily = False
+
+    if input_file_path == '0_example.txt':
+        is_example = True
+    elif input_file_path == '1_binary_landscapes.txt':
+        is_binary = True
+    elif input_file_path == '10_computable_moments.txt':
+        is_computable = True
+    elif input_file_path == '11_randomizing_paintings.txt':
+        is_random = True
+    elif input_file_path == '110_oily_portraits.txt':
+        is_oily = True
+    else:
+        raise Exception('Error:', 'Invalid input file')
+    res = main(input_file_path, is_binary, is_oily, is_random, is_computable, is_example)
     print(input_file_path, res)
     return res
 
@@ -15,5 +36,5 @@ for file in os.listdir('Data'):
     if file.endswith('.txt'):
         total += KCW_final_score(file)
 
-print('Total time:', time.time()-start)
+print('Total time:', (time.time()-start) / 60, ' minutes')
 print('Total score:', total)
